102ed58d56c8d7791466e28a58c0137b8aadd054
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2016-12-13 10:05:14 +0700

    add mobx-devtools

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import DevTools from 'mobx-react-devtools';
 
 import Navigation from './Navigation'
 import Content from './Content'
@@ -7,6 +8,7 @@ export default class App extends React.Component {
   render(){
     return (
       <div className="container">
+        <DevTools />
         <div className="col-xs-8 col-xs-offset-2">
           <div className="row">
             <Navigation />
